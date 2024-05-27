3da6d339592cbe7aed8b091b1488c9564a5e8fd5
Xylian <33781686+LuceusXylian@users.noreply.github.com>
2024-05-27 10:00:01 +0200

    `assert` keyword is deprecated for import fix

--- frontwork-std/test/test.i18n.ts
+++ frontwork-std/test/test.i18n.ts
@@ -1,6 +1,6 @@
 import { I18n, I18nLocale, I18nLocaleTranslation } from "../frontwork.ts";
-import { default as english } from "./i18n/english.json" assert { type: "json" };
-import { default as german } from "./i18n/german.json" assert { type: "json" };
+import { default as english } from "./i18n/english.json" with { type: "json" };
+import { default as german } from "./i18n/german.json" with { type: "json" };
 
 export const i18n = new I18n([
     new I18nLocale("en", english as I18nLocaleTranslation[]),
