769ded8c8cec217eab98d20f9ce321d92bbe3a1c
LuceusXylian <luceusxylian@gmail.com>
2025-09-26 20:13:00 +0200

    push v0.4.3

--- frontwork-cli/template/bundle.ts
+++ frontwork-cli/template/bundle.ts
@@ -1,5 +1,5 @@
 import { APP_CONFIG } from './src/environments/environment.ts';
-import { frontwork_bundler } from "https://deno.land/x/frontwork@0.4.2/frontwork-bundler.ts";
+import { frontwork_bundler } from "https://deno.land/x/frontwork@0.4.3/frontwork-bundler.ts";
 
 const distdir = Deno.args[0] || "dist/development-web";
 const distdir_js = distdir + "/js/";
--- frontwork-cli/template/src/dependencies.ts
+++ frontwork-cli/template/src/dependencies.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/frontwork@0.4.2/lib.ts";
+export * from "https://deno.land/x/frontwork@0.4.3/lib.ts";
